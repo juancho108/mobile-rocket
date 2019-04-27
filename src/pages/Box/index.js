00bf704ba8c56336c9482f7358b4309663094ead
juancho108 <jjf108@gmail.com>
2019-04-27 01:39:25 -0300

    proyecto mobile

---
+++ src/pages/Box/index.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
+
+import { distanceInWords } from "date-fns";
+import es from "date-fns/locale/es";
+
+import api from "../../services/api";
+
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+import styles from "./styles";
+
+export default class Box extends Component {
+  state = { box: {} };
+
+  async componentDidMount() {
+    //this.subscribeToNewFiles();
+
+    const box = await AsyncStorage.getItem("@RocketBox:box");
+    console.log(box);
+
+    const response = await api.get(`boxes/${box}`);
+
+    this.setState({ box: response.data });
+  }
+
+  renderItem = ({ item }) => {
+    <TouchableOpacity onPress={() => {}} style={styles.file}>
+      <View style={styles.fileInfo}>
+        <Icon name="insert-drive-file" size={24} color="#A5CFFF" />
+        <Text style={styles.fileTitle}>{item.title}</Text>
+      </View>
+
+      <Text style={styles.fileDate}>
+        Hace{" "}
+        {distanceInWords(item.createdAt, new Date(), {
+          locale: es
+        })}
+      </Text>
+    </TouchableOpacity>;
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.boxTitle}>JuancitoAPP</Text>
+
+        <FlatList
+          styles={styles.list}
+          data={this.state.box.files}
+          keyExtractor={file => file._id}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          renderItem={this.renderItem}
+        />
+      </View>
+    );
+  }
+}
---
+++ src/pages/Main/index.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
+
+import api from "../../services/api";
+
+import styles from "./styles";
+
+import logo from "../../assets/logo.png";
+
+export default class Box extends Component {
+  state = {
+    newBox: ""
+  };
+
+  async componentDidMount() {
+    const box = await AsyncStorage.getItem("@RocketBox:box");
+
+    if (box) {
+      this.props.navigation.navigate("Box");
+    }
+  }
+
+  handleSingIn = async () => {
+    const response = await api.post("boxes", {
+      title: this.state.newBox
+    });
+
+    await AsyncStorage.setItem("@RocketBox:box", response.data._id);
+
+    //averiguar mas sobre props.history
+    this.props.navigation.navigate("Box");
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image style={styles.logo} source={logo} />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Cree un Box"
+          placeholderTextColor="#999"
+          autoCorrect={false}
+          underlineColorAndroid="transparent"
+          value={this.state.newBox}
+          onChangeText={text => this.setState({ newBox: text })}
+        />
+
+        <TouchableOpacity onPress={this.handleSingIn} style={styles.button}>
+          <Text style={styles.buttontext}>Crear</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
